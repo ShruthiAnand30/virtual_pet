4206fab09b7c4ba1e8033e75632c593c1ebe1a12
ShruthiAnand30 <67490120+ShruthiAnand30@users.noreply.github.com>
2020-09-11 18:35:53 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+//Create variables here
+var dog,dogH;
+var database,food,foodStock;
+function preload()
+{
+  //load images here
+  dogImg = loadImage("images/dogImg.png");
+  dogHImg = loadImage("images/dogImg1.png");
+}
+
+function setup() {
+  createCanvas(500, 500);
+  
+  
+}
+
+
+function draw() {  
+
+  drawSprites();
+  //add styles here
+
+}
+
+
+
